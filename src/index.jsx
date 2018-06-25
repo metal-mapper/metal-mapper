fe45a55a9be0a6f19108b5fc9b5b42ba4654c6ed
Kirsty Devlin <kirstydevlin@Annas-MacBook-Pro.local>
2018-06-25 20:11:35 +0100

    Converted HTML to React and added NavBar file

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,27 @@ import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 
+import './styles/index.css';
+
 render(
-  (
-    <h1></h1>
+  (<div>
+    <div className="topnav">
+      <a className="active" href="#home">Home</a>
+      <a href="#BrowseLocation">Browse Locations</a>
+      <a href="#AddLocation">Add Location</a>
+    </div>
+    
+    <div className="middlenav">
+      <a href="#Register">Register</a>
+      <a href="#LogIn">Log In</a>
+    </div>
+    
+    <div style={{ paddingLeft: '16px' }}>
+      <h2>Metal Mapper Guide</h2>
+      <p>Search below for your perfect Metal Mapper trip:</p>
+    </div>
+  </div>
+
   ), document.getElementById('root'),
 );
 
