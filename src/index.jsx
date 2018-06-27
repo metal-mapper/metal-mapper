1d891b2d07a24c75d48aa1757ca6d589f627b4d7
Kirsty Devlin <kirstydevlin@Annas-MacBook-Pro.local>
2018-06-27 17:05:06 +0100

    Revert "addressed merge conflict in app.jsx"
    This reverts commit 7dc22874a41dfca42ccdcfc5dee7a5ed0b6184f4, reversing
    changes made to 847edef36be0cd679b62914246f70b7c7d9eb238.

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,5 @@ import { render } from 'react-dom';
 import App from './app';
 
 import './styles/index.css';
-// import { locationSearchBar } from './components/locationSearchBar'; 
 
 render(<App />, document.getElementById('root'));
