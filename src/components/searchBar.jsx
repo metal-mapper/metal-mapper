75a7eef91de5368434636fa95c3cb5a919b19b52
Kirsty Devlin <kirstydevlin@Annas-MacBook-Pro.local>
2018-06-29 15:43:23 +0100

    created searBar (not working, but nearly there

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A location has been submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter your nearest city:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Find Venues" />
+        </form>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <SearchBar />,
+    document.getElementById('root')
+  );
+
+export default SearchBar;
