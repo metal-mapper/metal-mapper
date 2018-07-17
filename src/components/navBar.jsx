6bdb5c8aa3a15bea52b2fe5ec0d108dd6c4c826f
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-07-17 21:50:31 +0100

    adding site logo

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 const NavBar = () => (
   <Router>
     <div className="topnav">
+      <img src="./images/metaltravelguidelogo" alt="metal travel guide logo" />
+      <h1>Metal Travel Guide</h1>
       <p> <Link to="/BrowseLocation">Browse Metal Venues</Link> </p>
       <p> <Link to="/AddVenue">Add A Metal Venue</Link> </p>
       <p> <Link to="/LogIn">Log In</Link> </p>
