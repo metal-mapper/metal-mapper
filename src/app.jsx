45a784f9f5f0a78d01016e44e262c4d32d8877f9
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-07-17 19:55:59 +0100

    thinning down the navbar and app

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import LogIn from './components/Login';
 import { Switch, Route } from 'react-router-dom';
-// import SearchBar from './components/SearchBar';
 import Venue from './components/Venue';
 import NavBar from './components/navBar';
 import AddVenue from './components/AddVenue';
@@ -23,23 +22,14 @@ class App extends React.Component {
     return (
       <div>
         <NavBar />
-        <div className="searchbar">
-          <h2>Enter your nearest city</h2>
-        </div>
-        <div className="middleimage">
-          <h2>Metal Mapper Guide</h2>
-          <p>Discover your perfect Metal Mapper experience!</p>
-        </div>
         {!this.state.user ?
-          <LogIn user={user} onSetUserState={this.handleSetUserState} /> :
+        <LogIn user={user} onSetUserState={this.handleSetUserState} /> :
           <Switch>
-            {/* <Route exact path="/" component={Home} /> */}
-            {/* <Route path="/browseVenues" component={BrowseVenues} /> */}
             <Route path="/addVenue" render={() => <AddVenue user={user} />} />
             <Route path="/login" component render={() => <LogIn user={user} onSetUserState={this.handleSetUserState} /> }/>
             <Route path="/venue" exact render={() => <Venue user={user} />} />
-            {/* <Route path="/venues" render={ () => <Venues user={user} />} /> */}
           </Switch>
+          <Footer />
         }
       </div>
     )
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,43 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-  </div>
-);
-
-const BrowseLocation = () => (
-  <div>
-    <h2>Browse</h2>
-  </div>
-);
-
-const AddLocation = () => (
-  <div>
-    <h2>Location</h2>
-  </div>
-);
-
-const LogIn = () => (
-  <div>
-    <LogIn />
-    <h2>Login page</h2>
-  </div>
-);
-
 const NavBar = () => (
   <Router>
     <div className="topnav">
-      <p> <Link to="/">Home</Link> </p>
-      <p> <Link to="/BrowseLocation">Browse Locations</Link> </p>
-      <p> <Link to="/AddLocation">Add Location</Link> </p>
+      <p> <Link to="/BrowseLocation">Browse Metal Venues</Link> </p>
+      <p> <Link to="/AddVenue">Add A Metal Venue</Link> </p>
       <p> <Link to="/LogIn">Log In</Link> </p>
     </div>
   </Router>
-
 );
-
-//test
 
 export default NavBar;
