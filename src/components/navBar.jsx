43924623df834d956c2fb8a8592fb3d4bdf624ca
Sophie Ashcroft <ms.sophie.ashcroft@gmail.com>
2018-07-18 18:34:34 +0100

    adding final bits and removing login component and image

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const NavBar = () => (
   <Router>
     <div className="topnav">
       {/* <img src="./images/metaltravelguidelogo" alt="metal travel guide logo" /> */}
       <h1>Metal Travel Guide</h1>
-      <p> <Link to="/BrowseLocation">Browse Metal Venues</Link> </p>
-      <p> <Link to="/AddVenue">Add A Metal Venue</Link> </p>
-      <p> <Link to="/LogIn">Log In</Link> </p>
+      <p className="left"> <Link to="/BrowseLocation">Browse Metal Venues</Link> </p>
+      <p className="left"> <Link to="/AddVenue">Add A Metal Venue</Link> </p>
+      <p className="left"> <Link to="/LogIn">Log In</Link> </p>
     </div>
   </Router>
 );
